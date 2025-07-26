d536c41cceea3bdae367efa3f0a174a67317f4af
Reuter Junior <reuter_junior@hotmail.com>
2025-07-26 16:51:38 -0300

    add: logo navbar

---
+++ src/components/Snackbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <transition name="fade">
+    <div v-if="visible" class="fixed bottom-5 right-5 bg-green-600 text-white px-4 py-2 rounded shadow-lg">
+      {{ message }}
+    </div>
+  </transition>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const props = defineProps<{
+  message: string
+  visible: boolean
+}>()
+</script>
+
+<style scoped>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.3s;
+}
+.fade-enter-from, .fade-leave-to {
+  opacity: 0;
+}
+</style>
