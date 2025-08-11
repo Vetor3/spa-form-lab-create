5242b2ccfc9f45c6daffb659174d61998bbebd70
Reuter Junior <reuter_junior@hotmail.com>
2025-08-11 17:36:45 -0300

    removido settings do imask

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,10 @@ import './style.css'
 import App from './App.vue'
 
 import { createPinia } from 'pinia'
-import { setupImask } from './plugins/imask';
 
 const app = createApp(App)
 const pinia = createPinia()
 
 app.use(pinia)
-
-setupImask(app);
 
 app.mount('#app')
