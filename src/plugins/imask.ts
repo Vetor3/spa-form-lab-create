6204fb8186220b52b78fefcbc75c76d5accb5774
Reuter Junior <reuter_junior@hotmail.com>
2025-08-11 17:35:21 -0300

    rm: arquivos do imask

--- src/plugins/imask.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue';
-import { IMaskDirective } from 'vue-imask';
-
-export function setupImask(app: ReturnType<typeof createApp>) {
-  app.directive('imask', IMaskDirective as any);
-} 
